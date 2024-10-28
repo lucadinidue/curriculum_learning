21d1ffa16a5caaf4c007ce97862d034d0d3d99fa
lucadinidue <luca.dini16@gmail.com>
2024-10-28 17:23:39 +0100

    added code for pos_tagging

--- scripts/eval/downstream_task_finetuning.py
+++ scripts/eval/downstream_task_finetuning.py
@@ -4,7 +4,9 @@ sys.path.append(os.path.abspath("."))
 
 from modules.custom_modeling_bert import BertForSentipolcClassification
 from transformers import (
+    DataCollatorForTokenClassification,
     AutoModelForSequenceClassification, 
+    AutoModelForTokenClassification,
     DataCollatorWithPadding,
     TrainingArguments, 
     AutoTokenizer, 
@@ -12,9 +14,11 @@ from transformers import (
 )
 
 from datasets import load_dataset
+from functools import partial
 import numpy as np
 import argparse
 import evaluate
+import ast
 
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -23,7 +27,38 @@ mae = evaluate.load('mae')
 spearmanr = evaluate.load("spearmanr")
 accuracy = evaluate.load("accuracy")
 f1 = evaluate.load("f1")
+seqeval = evaluate.load("seqeval")
 
+def compute_metrics_for_token_classification(p, label_list):
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=2)
+
+    # Remove ignored index (special tokens)
+    true_predictions = [
+        [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+
+    results = seqeval.compute(predictions=true_predictions, references=true_labels)
+    
+    # Unpack nested dictionaries
+    final_results = {}
+    for key, value in results.items():
+        if isinstance(value, dict):
+            for n, v in value.items():
+                final_results[f"{key}_{n}"] = v
+        else:
+            final_results[key] = value
+    final_results["precision"] = results["overall_precision"]
+    final_results["recall"] = results["overall_recall"]
+    final_results["f1"] = results["overall_f1"]
+    final_results["accuracy"] = results["overall_accuracy"]
+    return final_results
+   
 
 def compute_metrics_regression(eval_pred):
     logits, labels = eval_pred
@@ -67,20 +102,35 @@ TASKS_MAP = {
     'complexity': {'compute_metrics': compute_metrics_regression, 'num_labels': 1},
     'sentiment': {'compute_metrics': compute_metrics_sentipolc_classification, 'num_labels': 2},
     'coherence': {'compute_metrics': compute_metrics_classification, 'num_labels': 2},
+    'pos_tagging': {'compute_metrics': compute_metrics_for_token_classification, 'num_labels': 235}
 }
+
+def get_model(model_path, task, num_labels):
+    if task == 'sentiment':
+        model = BertForSentipolcClassification.from_pretrained(model_path, num_labels=num_labels)
+    elif task == 'pos_tagging':
+        model = AutoModelForTokenClassification.from_pretrained(model_path, num_labels=num_labels)
+    else:
+        model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=num_labels)
+    return model
+
+
+def get_gata_collator(tokenizer, task):
+    if task == 'pos_tagging':
+        data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+    else:
+        data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+    return data_collator
+
 
 
 def model_finetuning(model_path, downstream_task, num_labels, compute_metrics, output_path, epochs):
     tokenizer_path = 'models/bert_tokenizer'
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, add_prefix_space=True)
 
-    tokenized_dataset = prepare_dataset(tokenizer, downstream_task)
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-    if downstream_task == 'sentiment':
-        model = BertForSentipolcClassification.from_pretrained(model_path, num_labels=num_labels)
-    else:
-        model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=num_labels)
+    model = get_model(model_path, downstream_task, num_labels)
+    data_collator = get_gata_collator(tokenizer, downstream_task)
+    tokenized_dataset = tokenize_dataset(downstream_task, tokenizer, model)
 
     training_args = TrainingArguments(
         output_dir=output_path, 
@@ -96,6 +146,9 @@ def model_finetuning(model_path, downstream_task, num_labels, compute_metrics, o
         save_strategy='no' 
         )
 
+    if downstream_task == 'pos_tagging':
+        compute_metrics = partial(compute_metrics, label_list=model.config.id2label)
+
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -108,24 +161,88 @@ def model_finetuning(model_path, downstream_task, num_labels, compute_metrics, o
     trainer.train()
     trainer.save_state()
 
-def prepare_dataset(tokenizer, task):
+def tokenize_dataset(task, tokenizer, model):
+    dataset = load_dataset_from_csv(task)
+    if task == 'pos_tagging':
+        tokenized_dataset = tokenize_dataset_dataset_for_token_classification(dataset, tokenizer, model)
+    else:
+        tokenized_dataset = tokenize_dataset_for_sentence_classification(dataset, tokenizer)
+    return tokenized_dataset
+
+
+def load_dataset_from_csv(task):
     data_files = {'train': f'data/evaluation/{task}/train.csv', 
                   'test': f'data/evaluation/{task}/test.csv'}
     
     dataset = load_dataset('csv', data_files=data_files)
+    return dataset
 
+
+def tokenize_dataset_for_sentence_classification(dataset, tokenizer):
     def preprocess_function(examples):
         return tokenizer(examples['text'], truncation=True, padding=True, max_length=128)
     
     tokenized_dataset = dataset.map(preprocess_function, batched=True, desc="Running tokenizer on dataset")
     return tokenized_dataset
 
+def convert_elements_to_list(example):
+    example['text'] = ast.literal_eval(example['text'])
+    example['labels'] = ast.literal_eval(example['label'])
+    return example
 
+def get_label_list(labels):
+    unique_labels = set()
+    for label in labels:
+        unique_labels = unique_labels | set(label)
+    label_list = list(unique_labels)
+    label_list.sort()
+    label_list.append('UNK')
+    return label_list
+
+def tokenize_dataset_dataset_for_token_classification(dataset, tokenizer, model):
+    dataset = dataset.map(convert_elements_to_list, remove_columns=['label'])
+    
+    label_list = get_label_list(dataset['train']['labels'])
+    label_to_id = {l: i for i, l in enumerate(label_list)}
+
+    model.config.label2id = label_to_id
+    model.config.id2label = dict(enumerate(label_list))
+
+
+    def tokenize_and_align_labels(examples):
+        tokenized_inputs = tokenizer(
+            examples['text'],
+            padding=True,
+            truncation=True,
+            max_length=128,
+            is_split_into_words=True,
+            )
+        labels = []
+        for i, label in enumerate(examples['labels']):
+            word_ids = tokenized_inputs.word_ids(batch_index=i)
+            previous_word_idx = None
+            label_ids = []
+            for word_idx in word_ids:
+                if word_idx is None:
+                    label_ids.append(-100)
+                elif word_idx != previous_word_idx:
+                    label_id = label_to_id[label[word_idx]] if label[word_idx] in label_to_id else label_to_id['UNK']
+                    label_ids.append(label_id)
+                else:
+                    label_ids.append(-100)
+                previous_word_idx = word_idx
+
+            labels.append(label_ids)
+        tokenized_inputs["labels"] = labels
+        return tokenized_inputs
+    
+    tokenized_dataset = dataset.map(tokenize_and_align_labels, batched=True, remove_columns=['text','id'], desc="Running tokenizer on train dataset")
+    return tokenized_dataset
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--model_dir', type=str)
-    parser.add_argument('-t', '--downstream_task', type=str, choices=['complexity', 'sentiment', 'coherence'])
+    parser.add_argument('-t', '--downstream_task', type=str, choices=['complexity', 'sentiment', 'coherence', 'pos_tagging'])
     parser.add_argument('-e', '--epochs', type=int, default=10)
     args = parser.parse_args()
 
