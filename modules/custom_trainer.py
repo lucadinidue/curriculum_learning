39bcac8d01dfaaa0a00f2e4ebc045f7a8a439427
lucadinidue <luca.dini16@gmail.com>
2024-11-06 10:04:20 +0100

    added save on epoch end

--- modules/custom_trainer.py
+++ modules/custom_trainer.py
@@ -6,7 +6,7 @@ import torch
 class NoShuffleTrainer(Trainer):
     def _get_train_sampler(self) -> torch.utils.data.Sampler:
         return SequentialSampler(self.train_dataset)
-    
+
 
 class IncrementalStepsCheckpointCallback(TrainerCallback):
     def __init__(self, output_directory):
@@ -16,11 +16,11 @@ class IncrementalStepsCheckpointCallback(TrainerCallback):
         step = state.global_step
         control.should_save = False
 
-        if step <= 100000 and step % 10000 == 0:
+        if step <= 4000 and step % 400 == 0:
             control.should_save = True
 
-        elif step <= 1000000 and step % 100000 == 0:
-            control.should_save = True        
-
-        elif step > 1000000 and step % 1000000 == 0:
+        elif step > 4000 and step % 4000 == 0:
             control.should_save = True
+
+    def on_epoch_end(self, args, state, control, **kwargs):
+        control.should_save = True
