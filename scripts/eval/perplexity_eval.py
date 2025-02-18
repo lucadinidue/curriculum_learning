49d8d2b33f33badd0183fc04abf7432467a77fa3
lucadinidue <luca.dini16@gmail.com>
2025-02-18 16:01:51 +0100

    updated perplexity results

--- scripts/eval/perplexity_eval.py
+++ scripts/eval/perplexity_eval.py
@@ -22,12 +22,12 @@ def add_to_res_dict(res_dict, model, checkpoint, metric, score, dataset):
     res_dict['dataset'].append(dataset)
 
 
-def load_perplexity_df(src_dir, model_size, average_random=False):
+def load_perplexity_df(src_dir, model_size, model_seed, average_random=False):
     res_dict = {'model': [], 'checkpoint':[], 'metric': [], 'score':[], 'dataset':[]}
     for dataset in ['wikipedia', 'treebank']:
         dataset_dir = os.path.join(src_dir, dataset)
         for model_name in os.listdir(dataset_dir):
-            if model_size not in model_name:
+            if model_size not in model_name or f'{model_seed}_train' not in model_name:
                 continue
             model_dir = os.path.join(dataset_dir, model_name)
             for checkpoint_file_name in os.listdir(model_dir):
@@ -62,14 +62,17 @@ def plot_results(res_df, output_path, min_checkpoint=None):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model_size', type=str, choices=['small', 'medium', 'base'])
-    parser.add_argument('-o', '--output_path', type=str)
+    parser.add_argument('-d', '--model_size', default='medium', choices=['small', 'medium', 'base'])
+    parser.add_argument('-s', '--model_seed', type=int, choices=[42, 755, 995])
     parser.add_argument('-a', '--average_random', action='store_true')
     args = parser.parse_args()
 
     src_dir = 'data/perplexity/'
-    res_df = load_perplexity_df(src_dir, args.model_size, average_random=args.average_random)
-    plot_results(res_df, args.output_path)
+    output_path = f'results/seed_{args.model_seed}/bert_{args.model_size}'
+    if args.average_random:
+        output_path += '_avg_random'
+    res_df = load_perplexity_df(src_dir, args.model_size, args.model_seed, average_random=args.average_random)
+    plot_results(res_df, output_path)
 
 if __name__ == '__main__':
     main()
